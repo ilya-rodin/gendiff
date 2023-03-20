709e9ff1dc29c5a8815802f086bc8025d76b92f2
Ilya Rodin <i1.rodin@yadnex.ru>
2023-03-20 16:13:52 +0300

    add -f help info

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,16 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander';
+import { Command, Option } from 'commander';
 const program = new Command();
 
 program
   .name('gendiff')
+  .usage('[options] <filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0')
-  .parse(process.argv);
+  .version('0.8.0');
+
+program
+  .helpOption('-h --help', 'output usage information')
+  .addOption(new Option('-f --format <type>', 'output format'));
+
+program.parse(process.argv);
