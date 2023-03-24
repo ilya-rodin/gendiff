cadf7aab18f9d2b2a1e24f2ce4095622020f4002
Ilya Rodin <i1.rodin@yadnex.ru>
2023-03-24 17:29:47 +0300

    add parsers and YAML parse logic

--- src/app.js
+++ src/app.js
@@ -1,24 +1,8 @@
 const _ = require('lodash');
-const fs = require('node:fs');
-const path = require('path');
+const getParsedData = require('./parsers');
 
-const getDataPath = (fileName1, fileName2) => {
-  const absolutePath1 = path.resolve(process.cwd(), fileName1);
-  const absolutePath2 = path.resolve(process.cwd(), fileName2);
-
-  return [absolutePath1, absolutePath2];
-};
-
-const getParsedData = (fileName1, fileName2) => {
-  const [path1, path2] = getDataPath(fileName1, fileName2);
-  const data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
-  const data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
-
-  return [data1, data2];
-};
-
-const genDiff = (filepath1, filepath2) => {
-  const [data1, data2] = getParsedData(filepath1, filepath2);
+const genDiff = (filename1, filename2) => {
+  const [data1, data2] = getParsedData(filename1, filename2);
   const keys1 = _.sortBy(_.keys(data1));
   const keys2 = _.sortBy(_.keys(data2));
   const keys = _.union(keys1, keys2);
---
+++ src/parsers.js
@@ -0,0 +1,30 @@
+const yaml = require('js-yaml');
+const fs = require('fs');
+const path = require('path');
+
+const getDataPath = (fileName1, fileName2) => {
+  const absolutePath1 = path.resolve(process.cwd(), fileName1);
+  const absolutePath2 = path.resolve(process.cwd(), fileName2);
+
+  return [absolutePath1, absolutePath2];
+};
+
+const getParsedData = (fileName1, fileName2) => {
+  const file1Format = fileName1.split('.')[1];
+  const file2Format = fileName2.split('.')[1];
+
+  const [path1, path2] = getDataPath(fileName1, fileName2);
+
+  let data1;
+  let data2;
+  if (file1Format === 'yml' && file2Format === 'yml') {
+    data1 = yaml.load(fs.readFileSync(path1, 'utf-8'));
+    data2 = yaml.load(fs.readFileSync(path2, 'utf-8'));
+  } else {
+    data1 = JSON.parse(fs.readFileSync(path1, 'utf-8'));
+    data2 = JSON.parse(fs.readFileSync(path2, 'utf-8'));
+  }
+  return [data1, data2];
+};
+
+module.exports = getParsedData;
