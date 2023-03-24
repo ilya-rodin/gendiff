c76e0e9fbf67b1221299c2838bce44a08a85457f
Ilya Rodin <i1.rodin@yandex.ru>
2023-03-24 13:21:30 +0300

    change quotes to single

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-const { Command } = require("commander");
-const genDiff = require("../src/app");
+const { Command } = require('commander');
+const genDiff = require('../src/app');
 
 const program = new Command();
 
 program
-  .name("gendiff")
-  .argument("<filepath1>")
-  .argument("<filepath2>")
+  .name('gendiff')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   })
-  .description("Compares two configuration files and shows a difference.")
-  .version("0.8.0");
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.8.0');
 
 program
-  .helpOption("-h --help", "output usage information")
-  .option("-f --format <type>", "output format");
+  .helpOption('-h --help', 'output usage information')
+  .option('-f --format <type>', 'output format');
 
 program.parse();
