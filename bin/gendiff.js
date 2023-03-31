efe913210820a64acb8898dc040ac123f9db3184
Ilya Rodin <i1.rodin@yandex.ru>
2023-03-31 18:43:20 +0300

    add default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,16 +7,14 @@ const program = new Command();
 
 program
   .name('gendiff')
+  .description('Compares two configuration files and shows a difference.')
+  .version('1.0.0')
   .argument('<filepath1>')
   .argument('<filepath2>')
+  .option('-f, --format <type>', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
-    console.log(genDiff(filepath1, filepath2));
-  })
-  .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
-
-program
-  .helpOption('-h --help', 'output usage information')
-  .option('-f --format <type>', 'output format');
+    const option = program.opts();
+    console.log(genDiff(filepath1, filepath2, option.format));
+  });
 
 program.parse();
