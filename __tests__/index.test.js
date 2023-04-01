6b1fb5c183d805814dd1ab29a6d92c318af31beb
Ilya Rodin <i1.rodin@yandex.ru>
2023-04-01 15:10:30 +0300

    add .js to imports

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import genDiff from '../src/index';
+import genDiff from '../src/index.js';
 
 import { stylishOutput, jsonOutput, plainOutput } from '../__fixtures__/outputs';
 
