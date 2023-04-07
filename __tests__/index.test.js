ed758a28f58f182c3b7adb9f5e4c0b2071c931aa
Ilya Rodin <i1.rodin@yandex.ru>
2023-04-08 00:29:34 +0300

    remove redundant test case

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -13,12 +13,11 @@ const stylishOutput = readFixtureFile('stylishOutput.txt');
 const plainOutput = readFixtureFile('plainOutput.txt');
 const jsonOutput = readFixtureFile('jsonOutput.txt');
 
-test.each(['.json', '.yml'])('File Extentions Test $# %o', (extention) => {
+test.each(['.json', '.yml'])('File Extentions Test %#: format %o', (extention) => {
   const filepath1 = getFixturePath(`file1${extention}`);
   const filepath2 = getFixturePath(`file2${extention}`);
   expect(genDiff(filepath1, filepath2)).toBe(stylishOutput);
   expect(genDiff(filepath1, filepath2, 'stylish')).toBe(stylishOutput);
   expect(genDiff(filepath1, filepath2, 'plain')).toBe(plainOutput);
   expect(genDiff(filepath1, filepath2, 'json')).toBe(jsonOutput);
-  expect(genDiff(filepath1, filepath2, 'unknown')).toThrow();
 });
