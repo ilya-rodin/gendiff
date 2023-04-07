bdbd9714b11397963f4f053602c0cd03bbc62941
Ilya Rodin <i1.rodin@yandex.ru>
2023-04-08 00:24:03 +0300

    add comma

--- src/parsers.js
+++ src/parsers.js
@@ -8,6 +8,6 @@ export default (data, format) => {
     case 'yaml':
       return yaml.load(data);
     default:
-      throw new Error(`Unknown format ${format}`);
+      throw new Error(`Unknown format '${format}'`);
   }
 };
