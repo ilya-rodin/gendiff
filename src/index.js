f43240ecc36e946f8464e98f326ff2951d27a817
Ilya Rodin <i1.rodin@yandex.ru>
2023-04-08 00:23:37 +0300

    refactor main file. add abstraction

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { resolve, extname } from 'path';
-import { readFileSync } from 'fs';
 import { cwd } from 'process';
+import { readFileSync } from 'fs';
 import getParsedData from './parsers.js';
 import generateDiff from './generator.js';
 import format from './formatters/index.js';
@@ -9,21 +9,17 @@ const getAbsolutePath = (filepath) => resolve(cwd(), filepath);
 const getFormat = (filepath) => extname(filepath).substring(1);
 const readFile = (filepath) => readFileSync(filepath, 'utf-8');
 
-const getDataFromPath = (filepath) => {
-  const dataFormat = getFormat(filepath);
-  const data = readFile(getAbsolutePath(filepath));
-
-  return [data, dataFormat];
+const getData = (filepath, formatType) => {
+  const file = readFile(getAbsolutePath(filepath));
+  return getParsedData(file, formatType);
 };
 
 export default (filepath1, filepath2, formatType = 'stylish') => {
-  const [data1, format1] = getDataFromPath(filepath1);
-  const [data2, format2] = getDataFromPath(filepath2);
+  const format1 = getFormat(filepath1);
+  const format2 = getFormat(filepath2);
+  const data1 = getData(filepath1, format1);
+  const data2 = getData(filepath2, format2);
 
-  const diff = generateDiff(
-    getParsedData(data1, format1),
-    getParsedData(data2, format2),
-  );
-
+  const diff = generateDiff(data1, data2);
   return format(diff, formatType);
 };
