c372ff78f88b1a870c78587e2a2392e2a34bb14d
Ilya Rodin <i1.rodin@yandex.ru>
2023-04-08 01:15:04 +0300

    refactor vars names

--- src/index.js
+++ src/index.js
@@ -3,23 +3,23 @@ import { cwd } from 'process';
 import { readFileSync } from 'fs';
 import getParsedData from './parsers.js';
 import generateDiff from './generator.js';
-import format from './formatters/index.js';
+import formatDiff from './formatters/index.js';
 
 const getAbsolutePath = (filepath) => resolve(cwd(), filepath);
-const getFormat = (filepath) => extname(filepath).substring(1);
+const getExtention = (filepath) => extname(filepath).substring(1);
 const readFile = (filepath) => readFileSync(filepath, 'utf-8');
 
-const getData = (filepath, formatType) => {
-  const file = readFile(getAbsolutePath(filepath));
-  return getParsedData(file, formatType);
+const getData = (filepath) => {
+  const extention = getExtention(filepath);
+  const content = readFile(getAbsolutePath(filepath));
+
+  return getParsedData(content, extention);
 };
 
 export default (filepath1, filepath2, formatType = 'stylish') => {
-  const format1 = getFormat(filepath1);
-  const format2 = getFormat(filepath2);
-  const data1 = getData(filepath1, format1);
-  const data2 = getData(filepath2, format2);
+  const data1 = getData(filepath1);
+  const data2 = getData(filepath2);
 
   const diff = generateDiff(data1, data2);
-  return format(diff, formatType);
+  return formatDiff(diff, formatType);
 };
